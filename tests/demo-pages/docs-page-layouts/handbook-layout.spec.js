386197afce01fd3ec24322bb04a71e8c09363259
Abu Hurayra <hurayraiit@gmail.com>
2025-01-01 17:29:20 +0600

    reformat

--- tests/demo-pages/docs-page-layouts/handbook-layout.spec.js
+++ tests/demo-pages/docs-page-layouts/handbook-layout.spec.js
@@ -1,18 +1,18 @@
 //@ts-check
 
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test.describe('Handbook Layout', () => {
-    test('handbook layout should load properly @demo', async ({ page }) => {
-        await page.goto("https://demo.betterdocs.co/handbook-layout/");
+test.describe("Handbook Layout", () => {
+  test("handbook layout should load properly @demo", async ({ page }) => {
+    await page.goto("https://demo.betterdocs.co/handbook-layout/");
 
-        // Top section
-        await expect.soft(page.getByRole('link', { name: 'Getting Started 6 articles' })).toBeVisible();
-        await expect.soft(page.getByRole('link', { name: 'Subscription 6 articles' })).toBeVisible();
-        await expect.soft(page.getByRole('link', { name: 'Configurations 6 articles' })).toBeVisible();
+    // Top section
+    await expect.soft(page.getByRole("link", { name: "Getting Started 6 articles" })).toBeVisible();
+    await expect.soft(page.getByRole("link", { name: "Subscription 6 articles" })).toBeVisible();
+    await expect.soft(page.getByRole("link", { name: "Configurations 6 articles" })).toBeVisible();
 
-        // First category snapshot test
-        await expect.soft(page.locator('#page')).toMatchAriaSnapshot(`
+    // First category snapshot test
+    await expect.soft(page.locator("#page")).toMatchAriaSnapshot(`
           - heading "Getting Started" [level=2]
           - text: "6"
           - list:
@@ -35,17 +35,21 @@ test.describe('Handbook Layout', () => {
             - img
         `);
 
-        // Other category titles
-        await expect.soft(page.locator('article').filter({ hasText: 'Configurations 6 How To Get' }).getByRole('heading')).toBeVisible();
-        await expect.soft(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-        await expect.soft(page.getByRole('heading', { name: 'Setup Process' })).toBeVisible();
-        await expect.soft(page.locator('article').filter({ hasText: 'Subscription 6 How To Get' }).getByRole('heading')).toBeVisible();
-        await expect.soft(page.getByRole('heading', { name: 'Payment Process' })).toBeVisible();
-        await expect.soft(page.getByRole('heading', { name: 'Integrations' })).toBeVisible();
-        await expect.soft(page.getByRole('heading', { name: 'FAQ' })).toBeVisible();
-        await expect.soft(page.getByRole('heading', { name: 'Troubleshooting' })).toBeVisible();
-        await expect.soft(page.getByRole('heading', { name: 'Release Notes' })).toBeVisible();
-        await expect.soft(page.getByRole('heading', { name: 'Use Cases' })).toBeVisible();
-        await expect.soft(page.getByRole('heading', { name: 'Community & Support' })).toBeVisible();
-    });
+    // Other category titles
+    await expect
+      .soft(page.locator("article").filter({ hasText: "Configurations 6 How To Get" }).getByRole("heading"))
+      .toBeVisible();
+    await expect.soft(page.getByRole("heading", { name: "Installation" })).toBeVisible();
+    await expect.soft(page.getByRole("heading", { name: "Setup Process" })).toBeVisible();
+    await expect
+      .soft(page.locator("article").filter({ hasText: "Subscription 6 How To Get" }).getByRole("heading"))
+      .toBeVisible();
+    await expect.soft(page.getByRole("heading", { name: "Payment Process" })).toBeVisible();
+    await expect.soft(page.getByRole("heading", { name: "Integrations" })).toBeVisible();
+    await expect.soft(page.getByRole("heading", { name: "FAQ" })).toBeVisible();
+    await expect.soft(page.getByRole("heading", { name: "Troubleshooting" })).toBeVisible();
+    await expect.soft(page.getByRole("heading", { name: "Release Notes" })).toBeVisible();
+    await expect.soft(page.getByRole("heading", { name: "Use Cases" })).toBeVisible();
+    await expect.soft(page.getByRole("heading", { name: "Community & Support" })).toBeVisible();
+  });
 });
