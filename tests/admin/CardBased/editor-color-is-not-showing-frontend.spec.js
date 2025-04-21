7c6edf91f1ce28f45e2d2da8005fc4d5f1d1b55b
mrx-arafat <arafatmrx@gmail.com>
2025-04-21 16:36:34 +0600

    create doc then add color heading then check then delete the doc | Arafat fix

--- tests/admin/CardBased/editor-color-is-not-showing-frontend.spec.js
+++ tests/admin/CardBased/editor-color-is-not-showing-frontend.spec.js
@@ -1,15 +1,12 @@
-//Arafat
-
+// Arafat
 import { test, expect } from "@playwright/test";
 
-// Load environment variables
 const baseUrl = process.env.BASE_URL_MSF;
 
-test.describe("BetterDocs Analytics - Views Validation", () => {
+test.describe("Goto WP-Admin Load", () => {
   let adminContext, adminPage;
 
   test.beforeAll(async ({ browser }) => {
-    // Initialize the browser context and open the analytics page
     adminContext = await browser.newContext({
       storageState: "playwright/.auth/admin.json",
     });
@@ -18,33 +15,59 @@ test.describe("BetterDocs Analytics - Views Validation", () => {
   });
 
   test.afterAll(async () => {
-    // Close the browser context after all tests are done
     await adminContext.close();
   });
 
   test("Create, Publish, and Validate Docs", async () => {
-    // Navigate to the BetterDocs editor page
     await adminPage.goto(`${baseUrl}/wp-admin/post-new.php?post_type=docs`);
 
-    // Wait for the page to load
-    await adminPage.waitForTimeout(2000);
-
-    // Add a title to the document
     await adminPage.getByLabel("Add title").click();
     await adminPage.getByLabel("Add title").fill("Header Docs");
 
-    // Add a heading block
     await adminPage.getByLabel("Add block").click();
-    await adminPage.getByPlaceholder("Search").click();
     await adminPage.getByPlaceholder("Search").fill("head");
     await adminPage
       .getByRole("option", { name: "Heading", exact: true })
       .click();
     await adminPage.getByLabel("Block: Heading").fill("Heading-01");
 
-    // Open the text style options
-
     await adminPage.getByRole("button", { name: "Text", exact: true }).click();
     await adminPage.getByLabel("Vivid green cyan").click();
+
+    await adminPage.waitForTimeout(3000);
+
+    await adminPage
+      .getByRole("button", { name: "Publish", exact: true })
+      .click();
+    await adminPage
+      .getByLabel("Editor publish")
+      .getByRole("button", { name: "Publish", exact: true })
+      .click();
+
+    await Promise.all([
+      adminPage.waitForNavigation(),
+      adminPage
+        .locator('[data-testid="snackbar"]')
+        .getByRole("link", { name: "View Docs" })
+        .click(),
+    ]);
+    await adminPage.waitForLoadState();
+
+    await expect(
+      adminPage.locator("#betterdocs-single-content").getByText("Heading-01")
+    ).toBeVisible();
+    //also check this heading has this color #00d084
+    const headingColor = await adminPage.evaluate(() => {
+      const headingElement = document.querySelector(
+        "#betterdocs-single-content h2"
+      );
+      return window.getComputedStyle(headingElement).color;
+    });
+    expect(headingColor).toBe("rgb(0, 208, 132)"); // Check if the color is #00d084
+
+    await adminPage.getByRole("menuitem", { name: "Edit Docs" }).click();
+    await adminPage.getByRole("button", { name: "Move to trash" }).click();
+    await adminPage.getByText("Are you sure you want to move").click();
+    await adminPage.getByRole("button", { name: "Move to trash" }).click();
   });
 });
