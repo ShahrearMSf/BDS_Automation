53fdd8da9c09a14a9dcc339fe845a196ce592803
ShahrearMSf <shahrearmuammar@gmail.com>
2025-04-23 16:17:54 +0200

    Multiple kb Box layout widget

--- tests/widgets/FreeProBoth/multiple-kb-box-l.spec.js
+++ tests/widgets/FreeProBoth/multiple-kb-box-l.spec.js
@@ -7,36 +7,36 @@ test.describe('Encyclopedia Retro - Basic Interactions', () => {
 
   test('Main Content', async ({ page }) => {
     await expect(page.locator('#content')).toMatchAriaSnapshot(`
-      - link /September \\d+, \\d+ 5 Docs/:
-        - heading /September \\d+, \\d+/ [level=2]
-      - link /BCB \\d+ Docs/:
-        - heading "BCB" [level=2]
-      - link "Country Roads 5 Docs":
-        - heading "Country Roads" [level=2]
-      - link "SAS 4 Docs":
-        - heading "SAS" [level=2]
-      - link "betterdocs-category-icon Loyalty Management 4 Docs":
-        - img "betterdocs-category-icon"
-        - heading "Loyalty Management" [level=2]
-      - link /June \\d+ 3 Docs/:
-        - heading /June \\d+/ [level=2]
-      - link "Holiday 6 Docs":
-        - heading "Holiday" [level=2]
-      - link "betterdocs-category-icon এসএএস 1 Doc":
-        - img "betterdocs-category-icon"
-        - heading "এসএএস" [level=2]
-      - link "betterdocs-category-icon বিসিবি 2 Docs":
-        - img "betterdocs-category-icon"
-        - heading "বিসিবি" [level=2]
-      - link "betterdocs-category-icon A 2 Docs":
-        - img "betterdocs-category-icon"
-        - heading "A" [level=2]
-      - link "betterdocs-category-icon B 1 Doc":
-        - img "betterdocs-category-icon"
-        - heading "B" [level=2]
-      - link "betterdocs-category-icon Dabang 3 Docs":
-        - img "betterdocs-category-icon"
-        - heading "Dabang" [level=2]
+    - link /September \\d+, \\d+ 5 Docs/:
+      - heading /September \\d+, \\d+/ [level=2]
+    - link /BCB \\d+ Docs/:
+      - heading "BCB" [level=2]
+    - link "Country Roads 5 Docs":
+      - heading "Country Roads" [level=2]
+    - link "SAS 4 Docs":
+      - heading "SAS" [level=2]
+    - link "betterdocs-category-icon Loyalty Management 4 Docs":
+      - img "betterdocs-category-icon"
+      - heading "Loyalty Management" [level=2]
+    - link /June \\d+ 3 Docs/:
+      - heading /June \\d+/ [level=2]
+    - link "Holiday 6 Docs":
+      - heading "Holiday" [level=2]
+    - link "betterdocs-category-icon এসএএস 1 Doc":
+      - img "betterdocs-category-icon"
+      - heading "এসএএস" [level=2]
+    - link "betterdocs-category-icon বিসিবি 2 Docs":
+      - img "betterdocs-category-icon"
+      - heading "বিসিবি" [level=2]
+    - link "betterdocs-category-icon A 2 Docs":
+      - img "betterdocs-category-icon"
+      - heading "A" [level=2]
+    - link "betterdocs-category-icon B 1 Doc":
+      - img "betterdocs-category-icon"
+      - heading "B" [level=2]
+    - link "betterdocs-category-icon Dabang 3 Docs":
+      - img "betterdocs-category-icon"
+      - heading "Dabang" [level=2]
     `);
   });
 
