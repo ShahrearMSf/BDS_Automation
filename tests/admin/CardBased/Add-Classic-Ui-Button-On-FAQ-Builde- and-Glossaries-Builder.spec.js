fc743d83b682216836b79146ac7ef76b0100f157
ShahrearMSf <shahrearmuammar@gmail.com>
2025-05-13 12:47:39 +0200

    update the classic button card for FAQ and Glossaries

--- tests/admin/CardBased/Add-Classic-Ui-Button-On-FAQ-Builde- and-Glossaries-Builder.spec.js
+++ tests/admin/CardBased/Add-Classic-Ui-Button-On-FAQ-Builde- and-Glossaries-Builder.spec.js
@@ -12,13 +12,27 @@ test.describe("BetterDocs 'betterdocs-admin' Class To Be Loaded Only In Betterdo
     adminPage = await adminContext.newPage();
   });
 
-test('test classic button', async ({ page }) => {
+test('test FAQ classic button', async ({ page }) => {
+  await adminPage.goto(`${baseUrl}/wp-admin/admin.php?page=betterdocs-dashboard`); 
+  await adminPage.getByRole('link', { name: 'FAQ Builder' }).click();
+  await adminPage.getByRole('link', { name: 'Classic UI' }).click();
+  await adminPage.getByRole('heading', { name: 'FAQ Categories' }).click();
+  await expect(adminPage.locator('#col-left')).toMatchAriaSnapshot(`- heading "Add New FAQ Category" [level=2]`);
+  await adminPage.locator('#addtag').getByText('Name', { exact: true }).click();
+  await adminPage.locator('#addtag').getByText('Slug', { exact: true }).click();
+  await adminPage.getByText('Parent FAQ Category').click();
+});
+
+test('test Glossaries classic button', async ({ page }) => {
   await adminPage.goto(`${baseUrl}/wp-admin/admin.php?page=betterdocs-dashboard`); 
   await adminPage.getByRole('link', { name: 'Glossaries' }).click();
   await adminPage.getByRole('link', { name: 'Classic UI' }).click();
-  await adminPage.getByRole('link', { name: 'BetterDocs' }).click();
-  await adminPage.getByRole('link', { name: 'FAQ Builder' }).click();
-  await adminPage.getByRole('link', { name: 'Classic UI' }).click();
+  await adminPage.getByRole('heading', { name: 'Glossaries Terms' }).click();
+  await adminPage.getByRole('heading', { name: 'Add New Glossaries Term' }).click();
+  await adminPage.locator('#addtag').getByText('Name', { exact: true }).click();
+  await adminPage.locator('#addtag').getByText('Slug', { exact: true }).click();
+  await adminPage.getByText('Parent Glossaries Term').click();
+  await adminPage.locator('#addtag').getByText('Description', { exact: true }).click();
 });
 
 });
